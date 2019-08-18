dabed789c7c50fb41b3428fbe16cd0f82223f91d
jerem <jerem@DESKTOP-SFN5S70>
2019-08-18 11:18:39 -0500

    Beta Test v1.0

--- SERVERS/test/DataService.php
+++ SERVERS/test/DataService.php
@@ -1,7 +1,7 @@
 <?php
- namespace flippyflappy {include_once "DAO.php";class DataService {static function update($values) {$DAO = self::getObject();return $DAO->update($values);}
+ namespace test {include_once "DAO.php";class DataService {static function update($values) {$DAO = self::getObject();return $DAO->update($values);}
         static function getRecords($whereCol, $whereOperator, $where) {$DAO = self::getObject();return $DAO->retrieve($whereCol, $whereOperator, $where);}
         static function delete($id) {$DAO = self::getObject();return $DAO->delete($id);}
-        private static function getObject() {return new\flippyflappy\DAO();}
+        private static function getObject() {return new\test\DAO();}
     }
 }
