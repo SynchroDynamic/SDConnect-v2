733b0fb6471bdaa29a71e39b06a486bb6fb8ed93
jerem <jerem@DESKTOP-SFN5S70>
2019-08-18 00:50:22 -0500

    Debug and Cleanup

---
+++ SERVERS/FreshStart/DataService.php
@@ -0,0 +1,7 @@
+<?php
+ namespace FreshStart {include_once "DAO.php";class DataService {static function update($values) {$DAO = self::getObject();return $DAO->update($values);}
+        static function getRecords($whereCol, $whereOperator, $where) {$DAO = self::getObject();return $DAO->retrieve($whereCol, $whereOperator, $where);}
+        static function delete($id) {$DAO = self::getObject();return $DAO->delete($id);}
+        private static function getObject() {return new\FreshStart\DAO();}
+    }
+}
--- sd-admin/settings.php
+++ sd-admin/settings.php
@@ -1,5 +1,7 @@
 <?php
+
 use \Phppot\Member;
+
 session_start();
 if (!empty($_SESSION["userId"])) {
     require_once __DIR__ . '/login/class/Member.php';
@@ -15,6 +17,63 @@ if (!empty($_SESSION["userId"])) {
     include_once dirname(__DIR__) . '/inc/sd-config1.php';
     echo '<meta http-equiv="refresh" content="0;url=' . \SDC::URL . \SDC::SUBFOLDER . 'sd-admin/login/">';
 }
+
+$message = "";
+if (isset($_POST['update'])) {
+    $url = $_POST['url'];
+    $dbName = $_POST['dbName'];
+    $uName = $_POST['uName'];
+    $pass = $_POST['pass'];
+    $host = $_POST['host'];
+    $rPath = $_POST['rPath'];
+    $sub = $_POST['sub'];
+    $salt = $_POST['salt'];
+
+    //$message = "name=" . $dbName . " uName=" . $uName . " pass=" . $pass . " host=" . $host;
+    //echo $message;
+
+    $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/" . $sub . '/inc/sd-config1.php', 'w');
+
+    $config = '<?php ' . "\n";
+    $config .= 'class SDC {' . "\n";
+    $config .= '//URL' . "\n";
+    $config .= 'const URL = "' . $url . '";' . "\n";
+    $config .= '//Navigation' . "\n";
+    $config .= 'const ROOT_PATH = "' . $rPath . '";' . "\n";
+    $config .= 'const SUBFOLDER = "' . $sub . '";' . "\n";
+    $config .= 'const SERVER_FOLDER = "SERVERS/";' . "\n";
+    $config .= 'const CONTROLLER_PATH = "/GateController.php";' . "\n";
+    $config .= '//Data Connection' . "\n";
+    $config .= 'const IP = "' . $host . '";' . "\n";
+    $config .= 'const DATABASE_NAME = "' . $dbName . '";' . "\n";
+    $config .= 'const DB_USERNAME = "' . $uName . '";' . "\n";
+    $config .= 'const DB_PASS = "' . $pass . '";' . "\n";
+    $config .= '//Encryption' . "\n";
+    $config .= 'const SESS_CIPHER = "aes-128-cbc";' . "\n";
+    $config .= 'const KEYWORD = "' . $salt . '";' . "\n";
+    $config .= "}";
+
+    fwrite($fp, $config);
+    fclose($fp);
+
+
+    include_once dirname(__FILE__, 2) . '/inc/sd-config1.php';
+    include_once $rPath . "/" . $sub . "/SERVERS/SHARED/ServerDatabase.php";
+
+    $conn = new \ServerDatabase();
+    $conn->getConnection();
+    $status = $conn->send("select count(*) from information_schema.tables where table_schema = database();");
+    $conn->closeConnection();
+    $cout = $status->rowCount();
+    //echo "COUNT " . $cout;
+    if ($cout > 0) {
+        $_GET['install'] = null;
+        //echo '&url=' . $url . "/" . $sub . '">';
+        echo '<script>alert("Update Successful!");</script>';
+    } else {
+        echo '<script>alert("Install Error!");</script>';
+    }
+}
 ?>
 
 <!doctype html>
@@ -23,51 +82,16 @@ if (!empty($_SESSION["userId"])) {
     readfile(dirname(__FILE__) . "/inc/head.html");
     echo "<body onload='getPageName()'>";
     readfile(dirname(__FILE__) . "/inc/layout.html");
-    $message = "";
-    if (isset($_POST['install'])) {
-        $dbName = $_POST['dbName'];
-        $uName = $_POST['uName'];
-        $pass = $_POST['pass'];
-        $host = $_POST['host'];
-        $prefix = $_POST['prefix'];
-        $rPath = $_POST['rPath'];
-        $rDir = "/" . $_POST['rDir'];
-        $salt = $_POST['salt'];
+    include_once dirname(__DIR__) . '/inc/sd-config1.php';
 
-        $message = "name=" . $dbName . " uName=" . $uName . " pass=" . $pass . " host=" . $host . " prefix=" . $prefix;
-        echo $message;
-
-        $fp = fopen($_SERVER['DOCUMENT_ROOT'] . $rDir . '/inc/sd-config1.php', 'w');
-
-        $config = "<?php \n\n"
-                . "class SDC {\n\n"
-                . "//Navigation\n"
-                . "const ROOT_PATH = '$rPath';\n"
-                . "const SERVER_FOLDER = '/SERVERS/';\n"
-                . "const CONTROLLER_PATH = '/Controller/GateController.php';\n"
-                . "//Data Connection\n"
-                . "const IP = '$host';\n"
-                . "const DATABASE_NAME = '$dbName';\n"
-                . "const DB_USERNAME = '$uName';\n"
-                . "const DB_PASS = '$pass';\n"
-                . "//Encryption\n"
-                . "const SESS_CIPHER = 'aes-128-cbc';\n"
-                . "const KEYWORD = '$salt';\n\n"
-                . "}";
-
-        fwrite($fp, $config);
-        fclose($fp);
-
-
-        //include_once $rPath . $rDir . '/inc/sd-config1.php';
-        include_once $rPath . $rDir . "/SERVERS/SHARED/ServerDatabase.php";
-
-        $conn = new \ServerDatabase();
-        $conn->getConnection();
-        $status = $conn->send("select count(*) from information_schema.tables where table_schema = database();");
-        $conn->closeConnection();
-        echo $status->fetchColumn();
-    }
+    $url = \SDC::URL;
+    $dbName = \SDC::DATABASE_NAME;
+    $uName = \SDC::DB_USERNAME;
+    $pass = \SDC::DB_PASS;
+    $host = \SDC::IP;
+    $rPath = \SDC::ROOT_PATH;
+    $sub = \SDC::SUBFOLDER;
+    $salt = \SDC::KEYWORD;
     ?>
 
     <body>
@@ -76,70 +100,57 @@ if (!empty($_SESSION["userId"])) {
 
             <form class="text-center border border-light p-5" method="POST" action="">
 
-                <p class="h4 mb-4">SynchroDynamic REST API Creator Install</p>
+                <p class="h4 mb-4">SynchroDynamic Database Management System</p>
+                <div class="form-group row">
+                    <label  for="url" class="col-sm-2 col-form-label">URL</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control"id="url" name="url" value="<?php echo $url; ?>">
+                    </div>
+                </div>
                 <div class="form-group row">
                     <label for="dbName" class="col-sm-2 col-form-label">Database Name</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="dbName" name="dbName" placeholder="sd-admin">
+                        <input type="text" class="form-control" id="dbName" name="dbName" value="<?php echo $dbName; ?>">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="uName" class="col-sm-2 col-form-label">Username</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="uName" name="uName" placeholder="root">
+                        <input type="text" class="form-control" id="uName" name="uName" value="<?php echo $uName; ?>">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="pass" class="col-sm-2 col-form-label">Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" id="pass" name="pass" placeholder="pass">
+                        <input type="password" class="form-control" id="pass" name="pass" value="<?php echo $pass; ?>">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="host" class="col-sm-2 col-form-label">Database Host IP and Database Port String</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="host" name="host" value="127.0.0.1:3306">
+                        <input type="text" class="form-control" id="host" name="host" value="<?php echo $host; ?>">
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="prefix" class="col-sm-2 col-form-label">Table Prefix</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="prefix" name="prefix" value="sd_">
-                    </div>
-
-                </div>
-
                 <div class="form-group row">
                     <label for="rPath" class="col-sm-2 col-form-label">Root Directory</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="rPath" name="rPath" value="<?php echo $_SERVER['DOCUMENT_ROOT'];
-    ?>">
+                        <input type="text" class="form-control" id="rPath" name="rPath" value="<?php echo $rPath; ?>">
                     </div>
 
                 </div>
                 <div class="form-group row">
-                    <label for="rDir" class="col-sm-2 col-form-label">Root Directory</label>
+                    <label for="sub" class="col-sm-2 col-form-label">Sub-Folder</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="rDir" name="rDir" placeholder="the folder SD is installed(No slashes)">
+                        <input type="text" class="form-control" id="sub" name="sub" value="<?php echo $sub; ?>">
                     </div>
-
-                </div>
-                <div class="form-group row">
-                    <label for="salt" class="col-sm-2 col-form-label">Salt Keyword</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="salt" name="salt" placeholder="A unique sequence of characters">
-                    </div>
+                    <input type="hidden" class="form-control" id="salt" name="salt" value="<?php echo $salt; ?>">
 
                 </div>
 
                 <!-- Sign in button -->
-                <button class="btn btn-info btn-block my-4" type="submit" name="install">Install Now</button>
+                <button class="btn btn-info btn-block my-4" type="submit" name="update">Update Settings</button>
             </form>
 
-            <div class="alert alert-danger" role="alert">
-                This file will be maintained until you delete it. You can save a copy off your server, and delete it when you are sure
-                that everything is installed correctly. Lastly, Change the file permission of the /inc folder in the root directory.
-            </div>
         </main>
     </div>
 </div>
